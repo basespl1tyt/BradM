fa3a71d540d85c530ebd7101b87cfedb745869a8
David Pham <35905105+basespl1tyt@users.noreply.github.com>
2019-07-04 19:38:33 -0400

    Rework
    Sim tab with xplane p3d and dcs
    DCS page
    Tab for schedule img
    Flight simulation over Flight simulations
    Updated disc link
    1tb SSD > 250GB
    Hyperlink addons
    Tickets Tab

--- js/script.js
+++ js/script.js
@@ -9,3 +9,38 @@ $(function () {
         $(".show").fadeOut();
     });
 });
+
+$(function () {
+    "use strict";
+    $("#schedule").click(function () {
+        var $src = $(this).attr("src");
+        $(".show").fadeIn();
+        $(".img-show img").attr("src", $src);
+    });
+    $("span, .overlay").click(function () {
+        $(".show").fadeOut();
+    });
+});
+
+
+$('.counting').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+
+    {
+      duration: 3000,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+        //alert('finished');
+      }
+  
+    });  
+});
